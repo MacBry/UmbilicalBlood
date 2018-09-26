db3db656adc6ff07bf83ed7abff863f4be94d48f
Maciej Bryja <maciej.bryja@wp.pl>
2018-09-26 18:26:33 +0200

    fix hospital table printer

--- src/com/mac/bry/krew/pepowiny/utils/HospitalPrintProcesor.java
+++ src/com/mac/bry/krew/pepowiny/utils/HospitalPrintProcesor.java
@@ -8,20 +8,36 @@ import com.mac.bry.krew.pepowiny.entity.HospitalAdress;
 public class HospitalPrintProcesor {
 	
 	private static void printFirstFourLastHospitalRow() {
-		for(int i = 0 ; i <= 153; i++) {
+		for(int i = 0 ; i <= 157; i++) {
 			System.out.print("=");
 		}
 		System.out.println();
 	}
+	private static void printMidleLine() {
+		System.out.print("||");
+		for(int i = 0 ; i <=47; i ++ ) {
+			if(i == 7 || i == 15 ) {
+				System.out.print("||");
+			}
+			else System.out.print(" ");
+		}
+		
+		System.out.print("||");
+		for(int i = 0 ; i <= 101; i++) {
+			System.out.print("=");
+		}
+		System.out.print("||");
+		System.out.println();
+	}
 	
 	private static void printSecondHospitalRow() {
-		System.out.printf("|| %-10s || %-30s || %-100s ||","LP", "HOSPITAL NAME", "HOSPITAL ADRESS");
+		System.out.printf("|| %-5s || %-5s || %-30s || %-100s ||","LP","ID", "HOSPITAL NAME", "HOSPITAL ADRESS");
 		System.out.println();
 	}
 	
 	private static void printThirdHospitalRow() {
-		System.out.printf("|| %-10s || %-30s || %-20s || %-10s || %-5s || %-25s || %-10s || %-10s ||",
-				"", "", "CITY", "ZIP CODE", "P-FIX", "STREET", "HOUSE NUM.", "LOCAL NUM.");
+		System.out.printf("|| %-5s || %-5s || %-30s || %-20s || %-10s || %-5s || %-25s || %-10s || %-10s ||",
+				"", "", "", "CITY", "ZIP CODE", "P-FIX", "STREET", "HOUSE NUM.", "LOCAL NUM.");
 		System.out.println();
 	}
 	
@@ -29,14 +45,16 @@ public class HospitalPrintProcesor {
 	public static void printHospitalList(List<Hospital> hospitalList, List<HospitalAdress> hospitalAdressList) {
 		printFirstFourLastHospitalRow();
 		printSecondHospitalRow();
+		printMidleLine();
 		printThirdHospitalRow();
 		printFirstFourLastHospitalRow();
 		int lp=1;
 		for(int i = 0 ; i < hospitalList.size() ; i++) {
 			for(int j = 0 ; j < hospitalAdressList.size(); j++) {
 				if(hospitalList.get(i).getId() == hospitalAdressList.get(j).getId()) {
-					System.out.printf("|| %-10d || %-30s || %-20s || %-10s || %-5s || %-25s || %-10s || %-10s ||",
+					System.out.printf("|| %-5d || %-5d || %-30s || %-20s || %-10s || %-5s || %-25s || %-10s || %-10s ||",
 							lp,
+							hospitalList.get(i).getId(),
 							hospitalList.get(i).getHostpitalName(),
 							hospitalAdressList.get(j).getCity(),
 							hospitalAdressList.get(j).getZipCode(),
