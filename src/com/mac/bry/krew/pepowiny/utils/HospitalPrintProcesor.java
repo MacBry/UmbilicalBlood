e0688531857c6e170cc85c66f2021efb7f943fde
Maciej Bryja <maciej.bryja@wp.pl>
2018-09-29 17:45:17 +0200

    finishing mother table printer

--- src/com/mac/bry/krew/pepowiny/utils/HospitalPrintProcesor.java
+++ src/com/mac/bry/krew/pepowiny/utils/HospitalPrintProcesor.java
@@ -67,7 +67,6 @@ public class HospitalPrintProcesor {
 			lp++;
 			System.out.println();
 		}
-		
 		printFirstFourLastHospitalRow();
 	}
 }
--- src/com/mac/bry/krew/pepowiny/utils/MotherPrintProcesor.java
+++ src/com/mac/bry/krew/pepowiny/utils/MotherPrintProcesor.java
@@ -2,10 +2,15 @@ package com.mac.bry.krew.pepowiny.utils;
 
 import java.util.List;
 
+import com.mac.bry.krew.pepowiny.DButils.DBHospitalUtility;
+import com.mac.bry.krew.pepowiny.entity.Hospital;
+import com.mac.bry.krew.pepowiny.entity.HospitalAdress;
 import com.mac.bry.krew.pepowiny.entity.Mother;
 import com.mac.bry.krew.pepowiny.entity.MotherAdress;
 
 public class MotherPrintProcesor {
+	
+	private static DBHospitalUtility dbHospitalUtility = new DBHospitalUtility();
 	
 	private static void printFirstFourLastMotherRow() {
 		for(int i = 0 ; i <= 209; i ++) {
@@ -41,12 +46,44 @@ public class MotherPrintProcesor {
 		System.out.println();
 	}
 	
-	public static void printMotherList(List<Mother> mother, List<MotherAdress> motherAdress) {
+	public static void printMotherList(List<Mother> mothersList, List<MotherAdress> mothersAdressList) {
+		List<Hospital> hospitalsList = dbHospitalUtility.getAllHospital();
+		List<HospitalAdress> hospitalsAdressList = dbHospitalUtility.getAllHospitalAdress();
 		printFirstFourLastMotherRow();
 		printSecondMotherRow();
 		printMidleLine();
 		printThirdMotherRow();
 		printFirstFourLastMotherRow();
 		int lp=1;
+		for(int i = 0 ; i < mothersList.size() ; i++) {
+			for(int k = 0 ; k < mothersAdressList.size() ; k++) {
+				for(int j = 0 ; j < hospitalsList.size() ; j++) {
+					for(int m = 0 ; m < hospitalsAdressList.size() ; m++) {
+						if(mothersList.get(i).getId() == mothersAdressList.get(k).getId() 
+								&& mothersList.get(i).getHospital().getId() == hospitalsList.get(j).getId()
+								&& hospitalsList.get(j).getId() == hospitalsAdressList.get(m).getId());
+						
+						System.out.printf("|| %-5s || %-5s || %-14s || %-14s || %-14s || %-20s || %-10s || %-5s || %-25s || %-10s || %-10s || %-12s || %-12s ||",
+								lp,
+								mothersList.get(i).getId(),
+								mothersList.get(i).getSurname(),
+								mothersList.get(i).getName(),
+								mothersList.get(i).getPESEL(),
+								mothersAdressList.get(k).getCity(),
+								mothersAdressList.get(k).getZipCode(),
+								mothersAdressList.get(k).getPrefix(),
+								mothersAdressList.get(k).getStreet(),
+								mothersAdressList.get(k).getHouseNumber(),
+								mothersAdressList.get(k).getLocalNumber(),
+								hospitalsList.get(j).getId(),
+								hospitalsAdressList.get(j).getId());
+					}
+				}
+			}
+			lp++;
+			System.out.println();
+		}
+		printFirstFourLastMotherRow();
 	}
+	
 }
